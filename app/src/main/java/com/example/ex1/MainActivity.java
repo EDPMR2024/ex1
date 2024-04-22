e9f79ca8974e74d9c7109a55f656ad98420fa00b
Thomas Bourdeaud'huy <thomas.bourdeaudhuy@gmail.com>
2024-04-22 15:33:29 +0200

    v1 : ajout d'une trace d'exécution dans MainActivity

--- app/src/main/java/com/example/ex1/MainActivity.java
+++ app/src/main/java/com/example/ex1/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.ex1;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +11,12 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String PMR = "pmr2024";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(PMR, "onCreate");
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
