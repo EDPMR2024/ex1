cc7384fd605feac94ede1e7c927123068076446f
Thomas Bourdeaud'huy <thomas.bourdeaudhuy@gmail.com>
2024-04-22 17:10:33 +0200

    v5 : click sur bouton

--- app/src/main/java/com/example/ex1/MainActivity.java
+++ app/src/main/java/com/example/ex1/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.ex1;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -10,12 +14,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     // TODO: définition d'une catégorie de messages
     // TODO : affiche la console logcat et filter
     //  suivant les occurrences du mot-clé "pmr2024"
     private static final String PMR = "pmr2024";
+    Button refBtnOK;
+    EditText refEdtNom;
 
     void alerter(String s) {
         Log.i(PMR, s);
@@ -30,9 +36,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // TODO: affichage d'un message
-        alerter("onCreate");
+        //alerter("onCreate");
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        refBtnOK= findViewById(R.id.button);
+        refEdtNom= findViewById(R.id.edtNom);
+        refBtnOK.setOnClickListener(this);
+        refEdtNom.setOnClickListener(this);
+    /*
+        refBtnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alerter("onclick2");
+            }
+        });
+
+     */
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -43,18 +62,40 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        alerter("onStart");
+        //alerter("onStart");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        alerter("onRestart");
+        //alerter("onRestart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        alerter("onResume");
+        //alerter("onResume");
     }
+
+
+    @Override
+    public void onClick(View view) {
+        alerter(Integer.toString(view.getId()));
+        /*switch(view.getId()) {
+            case R.id.edtNom:
+                alerter("click sur edt");
+                break;
+
+            case R.id.button:
+                alerter("click sur bouton");
+                break;
+
+        }*/
+    }
+
+    /*
+    public void foo(View view) {
+        alerter("click");
+    }
+    */
 }
