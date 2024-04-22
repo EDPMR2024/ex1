961b5bb51dd3a2e2be3d39ae6a5a6a2c58616b55
Thomas Bourdeaud'huy <thomas.bourdeaudhuy@gmail.com>
2024-04-22 15:47:01 +0200

    v1 : ajout d'une trace d'exécution dans MainActivity

--- app/src/main/java/com/example/ex1/MainActivity.java
+++ app/src/main/java/com/example/ex1/MainActivity.java
@@ -11,11 +11,16 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    // TODO: définition d'une catégorie de messages
+    // TODO : affiche la console logcat et filter
+    //  suivant les occurrences du mot-clé "pmr2024"
     private static final String PMR = "pmr2024";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // TODO: affichage d'un message
         Log.i(PMR, "onCreate");
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
